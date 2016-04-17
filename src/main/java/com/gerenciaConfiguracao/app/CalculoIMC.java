3c7fd619b05a71860ca60b7a34b4bf532ed2ab35
Usuario <Usuario>
2016-04-17 15:52:52 -0300

    Commit do projeto Develop

---
+++ src/main/java/com/gerenciaConfiguracao/app/CalculoIMC.java
@@ -0,0 +1,17 @@
+public class CalculoIMC {
+
+	public static void main(String[] args) {
+
+
+		double altura = 1.9;
+		int peso = 77;
+		
+		double resultado = peso/(altura*altura);
+		
+		System.out.println("Seu IMC é: " + resultado);
+		
+		
+
+	}
+
+}
